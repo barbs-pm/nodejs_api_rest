4ccfd86697efc94d9fd4f480f89ed8e10e2d1b51
barbs-pm <barbarapmarkus@gmail.com>
2022-02-05 18:43:47 -0300

    adicionada validação ao atendimento

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -7,16 +7,39 @@ class Atendimento {
         const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
         const atendimentoDatado = {...atendimento, dataCriacao, data}
+        
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+        const clienteEhValido = atendimento.cliente.length >= 5
 
-        const sql = 'INSERT INTO Atendimentos SET ?'
-
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if(erro) {
-                res.status(400).json(erro)
-            } else {
-                res.status(201).json(resultados)
+        const validacoes = [
+            {
+                nome: 'data',
+                valido: dataEhValida,
+                mensagem: 'A data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: clienteEhValido,
+                mensagem: 'Insira o nome completo do cliente'
             }
-        })
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if(existemErros) {
+          res.status(400).json(erros)  
+        } else {
+            const sql = 'INSERT INTO Atendimentos SET ?'
+
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro) {
+                    res.status(400).json(erro)
+                } else {
+                    res.status(201).json(resultados)
+                }
+            })
+        }        
     }
 }
 
